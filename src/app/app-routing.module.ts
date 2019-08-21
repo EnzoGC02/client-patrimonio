3742146c0395de76f02d8d5c9ad35f7bbb023545
EnzoGC02 <Administrador@deportes.gob>
2019-08-21 12:13:50 -0300

    elementos y sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ElementosComponent} from './components/elementos/elementos.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'elementos/add',
+    component:ElementosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
